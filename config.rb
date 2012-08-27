c6aa40c406dff03ca87d40984b50bf8e3b357953
Frank Boes <frank.boes@bluemars.net>
2012-08-27 17:37:43 +0200

    Improved configuration

--- config.rb
+++ config.rb
@@ -1,46 +1,14 @@
-#
-# This file is only needed for Compass/Sass integration. If you are not using
-# Compass, you may safely ignore or delete this file.
-#
-# If you'd like to learn more about Sass and Compass, see the sass/README.txt
-# file for more information.
-#
+# This is the configuration file for Compass (http://compass-style.org/)
+# See http://compass-style.org/help/tutorials/configuration-reference/ on how to use this file
 
-# Change this to :production when ready to deploy the CSS to the live server.
-#environment = :development
-environment = :production
+#environment     = :development
+environment     = :production
 
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = (environment == :development)
-
-
-# Location of the theme's resources.
 css_dir         = "css"
 sass_dir        = "sass"
 images_dir      = "css"
-#javascripts_dir = "js"
-#extensions_dir  = "sass-extensions"
-
-
-# Require any additional compass plugins here.
-#require 'plugin'
-
-##
-## You probably don't need to edit anything below this.
-##
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compact
-
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server root.
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
+# The output style for the compiled css. One of: :nested, :expanded, :compact, or :compressed.
+output_style    = (environment == :development) ? :expanded : :compact
+sass_options    = (environment == :development) ? {:debug_info => true} : {}
