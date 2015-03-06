5db60d6abe5c5d1e8c6ae34a87c7cbed391678f3
Frank Boës <frank.boes@shift.agency>
2015-03-06 15:31:37 +0100

    Adding more interaction examples

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,7 +1,7 @@
 /*global $ */
 (function ($) {
 	'use strict';
-	$.fn.exampleInteraction = function () { // function (options)
+	$.fn.bodyInteraction = function () { // function (options)
 		return this.each(function() {
 			var main = {
 				/*options : $.extend({},options),*/
@@ -15,7 +15,61 @@
 
 					// Your stuff here
 
-					this.elements.parent.on('click.exampleInteraction', function (event) {
+					this.elements.parent.on('click.body', '.js-body-example', function (event) {
+						event.preventDefault();
+						event.stopPropagation();
+						// Your stuff here
+						// console.log($(this));
+					});
+				}
+			};
+
+			main.init($(this));
+		});
+	};
+
+	$.fn.navigationInteraction = function () { // function (options)
+		return this.each(function() {
+			var main = {
+				/*options : $.extend({},options),*/
+				elements : {
+					parent : null
+				},
+				/*values : {},*/
+				init : function (el) {
+					//var that = this;
+					this.elements.parent = el;
+
+					// Your stuff here
+
+					this.elements.parent.on('click.navigation', 'a', function (event) {
+						event.preventDefault();
+						event.stopPropagation();
+						// Your stuff here
+						// console.log($(this));
+					});
+				}
+			};
+
+			main.init($(this));
+		});
+	};
+
+	$.fn.mainInteraction = function () { // function (options)
+		return this.each(function() {
+			var main = {
+				/*options : $.extend({},options),*/
+				elements : {
+					parent : null
+				},
+				/*values : {},*/
+				init : function (el) {
+					//var that = this;
+					this.elements.parent = el;
+
+					// Your stuff here
+
+					this.elements.parent.on('click.main', '.js-main-demo', function (event) {
 						event.preventDefault();
 						event.stopPropagation();
 						// Your stuff here
@@ -31,5 +85,7 @@
 
 $(document).ready(function() {
 	'use strict';
-	$('.js-exampleInteraction').exampleInteraction();
+	$('body').bodyInteraction(); // ({})
+	$('#navigation').navigationInteraction(); // ({})
+	$('#main').mainInteraction(); // ({})
 });
