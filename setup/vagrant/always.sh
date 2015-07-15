ff21c2a95e1fb07af0067b16cc664de0ea251510
Frank Boës <frank.boes@shift.agency>
2015-07-15 12:28:11 +0200

    Installing SASS

---
+++ setup/vagrant/always.sh
@@ -0,0 +1,2 @@
+cd /var/www
+grunt watch
--- setup/vagrant/bootstrap.sh
+++ setup/vagrant/bootstrap.sh
@@ -1,5 +1,6 @@
 sudo sed -i -- 's/\/public/\/htdocs/g' /etc/apache2/**/*.conf
 sudo service apache2 restart
+sudo apt-get install ruby -y
+sudo su -c "gem install sass"
 cd /var/www
 npm install
-grunt watch
