e86238f47733d25ab5da2193a94fbabb5ed10045
Frank Boës <frank.boes@shift.agency>
2015-07-16 15:45:18 +0200

    Scotch.box 2.0

--- setup/vagrant/bootstrap.sh
+++ setup/vagrant/bootstrap.sh
@@ -1,3 +1,5 @@
+#sudo apt-get update
+#sudo apt-get upgrade -y
 sudo apt-get install language-pack-de-base -y
 sudo sed -i -- 's/\/public/\/htdocs/g' /etc/apache2/**/*.conf
 sudo service apache2 restart
@@ -5,3 +7,4 @@ sudo apt-get install ruby -y
 sudo su -c "gem install sass"
 cd /var/www
 npm install
+# mysql -u root -proot scotchbox < setup/dump.sql
