4c54ef5a691448da6a5874401afa6b95daf49bba
Frank Boës <frank.boes@shift.agency>
2015-09-01 10:47:11 +0200

    Adding data dumper

--- setup/dbdump.sh
+++ setup/dbdump.sh
@@ -2,3 +2,4 @@
 cd ${0%/*}/..
 
 mysqldump -u root -p --no-data --skip-comments --add-drop-table broilerplate > setup/mysql/dbdump.sql
+# mysqldump -u root -p  --no-create-info --skip-comments --insert-ignore broilerplate {{ DB_TABLE }} >> setup/mysql/dbdump.sql
