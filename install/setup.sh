0a6e031e26e838fbe9560bb55026f6d36a9e5c93
Frank Boës <frank.boes@shift.agency>
2015-10-19 11:18:03 +0200

    Improved setup of writable directories

--- install/setup.sh
+++ install/setup.sh
@@ -10,21 +10,26 @@ if [ -f install/mysql/dbdump.sql ]; then
 	install/import-dbdump.sh
 fi
 
-cd htdocs
+function make_writable_directory {
+	mkdir -p $1 && chmod 777 $1
+	if [ -x "/usr/sbin/sestatus" ]; then
+		echo "semanage fcontext -a -t httpd_sys_rw_content_t \"$CWD/$1(/.*)?\""
+	fi
+}
+
 # mkdir -p tmp
-# mkdir -p uploads && chmod 777 uploads
+# make_writable_directory htdocs/files
 #[ -h TARGET ] || ln -s SOURCE TARGET
 #[ -f TARGET ] || cp SOURCE TARGET
-cd ..
 
 if [ ! -d /vagrant ]; then
 	echo ""
-	echo -e "=== [32mApache2 vhost config[m ==="
+	echo -e "=== \x1B[32mApache2 vhost config\x1B[m ==="
 	echo ""
 	sed "s#/var/www#$CWD#g" install/apache/macro-broilerplate.conf
 	sed "s#/var/www#$CWD#g" install/apache/broilerplate.local.conf
 	echo ""
-	echo -e "=== [32m/etc/hosts[m === "
+	echo -e "=== \x1B[32m/etc/hosts\x1B[m === "
 	echo ""
 	echo "127.0.0.1    broilerplate.local"
 	echo ""
